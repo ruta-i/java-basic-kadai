3e5acfe35dbb536de541aa460636eaa63506dada
ruta-i <130815861+ruta-i@users.noreply.github.com>
2024-04-20 08:34:43 +0900

    kadai_015をアップロード

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,13 @@
+package kadai_015;
+
+public class CarExec_Chapter15 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Car_Chapter15 gear3 = new Car_Chapter15( 1,10 );
+		
+		gear3.gearChange(3);
+		gear3.run();
+	}
+
+}
---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,36 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+
+	// フィールドを定義（内部データ）
+	private int  gear = 1; // 1速から5速のギアを表す
+	private int speed = 10; // ギアチェンジ後の速度を表す
+	
+	//コンストラクタ
+	public Car_Chapter15( int gear, int speed ) {
+		this.gear = gear;
+	}
+	
+	// ギアの値により速度を変えるメソッド
+	int afterGear;
+	public void gearChange( final int afterGear ) {
+		// switch文により、ギアに応じた速度を定義する
+		switch(afterGear) {
+		case 1 -> speed = 10;
+		case 2 -> speed = 20;
+		case 3 -> speed = 30;
+		case 4 -> speed = 40;
+		case 5 -> speed = 50;
+		default -> speed = 10; 
+		}
+		
+		System.out.println("ギア" + gear + "から" + afterGear + "に切り替えました");
+		
+		int gear = afterGear;
+	}
+	
+	public void run() {
+		System.out.println("速度は時速" + speed + "kmです");
+	}
+	
+}
